80ea52b9131ed9e7ffa7e0eae6b225ad570222bc
Marvin Navarrete <ng23001@itcha.edu.sv>
2024-05-26 10:10:45 -0600

    agregacion de modelos, migraciones, controladores dashboard

---
+++ app/Models/Equipo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Equipo extends Model
+{
+    use HasFactory;
+    public function inventario_productos(){
+        return $this->hasMany(InventarioEquipo::class);
+    }
+}
---
+++ database/migrations/2024_05_25_042710_create_inventario_equipos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventario_equipos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 80);
+            $table->decimal('peso', 8,2);
+            $table->decimal('cantidad', 8,2 );
+            $table->unsignedBigInteger('equipo_id');;
+            $table->foreign('equipo_id')->references('id')->on('equipos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventario_equipos');
+    }
+};
