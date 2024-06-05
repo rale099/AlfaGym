488cfcb2404990e24c18256b0eb1e15fbbf07ceb
Zan <hf23001@itcha.edu.sv>
2024-06-05 13:02:48 -0600

    mapa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,19 +3,27 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard Administrativo</h1>
+    <h1>Lugar del establecimiento</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
-
-@section('css')
-    <!--<link rel="stylesheet" href="/css/admin_custom.css"> -->
-    @vite('resources/css/app.css')
+    <div id="map-container" style="height: 400px; width: 100%; margin: 0 auto;"></div> <!-- Contenedor para el mapa -->
 @stop
 
 @section('js')
-    <!-- <script> console.log('Hi!'); </script> -->
-    @vite('resources/js/app.js')
+    <!-- Script para cargar la API de Google Maps -->
+    <script async  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxvNn7ZfiUUQgyQqXapbCOvChKJ_FMzgM&callback=initMap"></script>
+
+    <!-- Script para inicializar el mapa -->
+    <script>
+        
+            function initMap() {
+    var map = new google.maps.Map(document.getElementById('map-container'), {
+        center: { lat: -16.0704139, lng: -65.5969469 },
+        zoom: 6
+    });
+}
+
+        
+    </script>
 @stop
