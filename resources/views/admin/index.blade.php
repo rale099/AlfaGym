1c8b91fd674421eb94351dae2c72e7b0d08142fd
Zan <hf23001@itcha.edu.sv>
2024-06-09 13:59:26 -0600

    creacion del mapa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -19,7 +19,7 @@
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map-container'), {
                 center: { lat: 13.960088235421136, lng: -89.18612534860371 },
-                zoom: 30
+                zoom: 29
             });
         }
     </script>
